75c9af31f6b6a9b67f3175bbdaa36cff9b87d7d2
jonasfeld <29547401+jonasfeld@users.noreply.github.com>
2023-11-15 16:56:58 +0100

    first commit

---
+++ lua/jonasfeld/init.lua
@@ -0,0 +1,6 @@
+require("jonasfeld.remap")
+require("jonasfeld.lazy")
+require("jonasfeld.treesitter")
+require("jonasfeld.lspzero")
+vim.opt.termguicolors = true
+vim.cmd.colorscheme('ayu-mirage')
---
+++ lua/jonasfeld/remap.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>f", vim.cmd.Ex)
+
+-- Move lines in visual mode
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "Y", "\"+y")
+
+-- Cursor stays in the middle of the screen when iterating over search terms
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Cursor stays in the middle of the screen when using <C-d> and <C-u>
+vim.keymap.set("n", "<C-d>", "<C-d>zzzv");
+vim.keymap.set("n", "<C-u>", "<C-u>zzzv");
+
+vim.keymap.set("x", "<leader>p", "\"_DP")
+
+vim.keymap.set("n", "Q", "<nop")
+vim.keymap.set("n", "Q", "<nop")
+
